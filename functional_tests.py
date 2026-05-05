358020bbe38608546e1d82d544fbca1edce8b0fc
pho3n1xy <echevarria.luis888@gmail.com>
2026-05-04 22:02:18 -0500

    we made changes to functional tests.

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,33 @@
 from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
-
-assert "Congratulations!" in browser.title
-print("OK")
+import unittest 
 
 
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_start_a_todo_list(self):
+        # She notices the page title and header mention to-do lists
+        assert "To-Do" in browser.title
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "reach englightment" into a text box
+        # Lane's hobby is ascending past the spiritual plane
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Reach Enlightenment" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item
+        # She enters "Escape from all Karmic debt" 
+
+        # The page updates again, and now shows both items on her list
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == "__main__":
+    unittest.main()
